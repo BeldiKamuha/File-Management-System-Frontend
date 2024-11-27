b34715192be3d102222ce7235ccbd9bf01ae36b7
Beldi Kamuha <beldikamuha@Beldis-MacBook-Air.local>
2024-11-27 21:11:34 +0300

    Importing Bootstrap and Bootstrap-Vue CSS files

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+// Import Bootstrap and Bootstrap-Vue CSS files
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// Import Bootstrap-Vue
+import BootstrapVue from 'bootstrap-vue';
+
+// Use Bootstrap-Vue
+Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app');
